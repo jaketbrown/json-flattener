6973a19144689e4f8d77e66acba7c4b60f77a0aa
Jake Brown <44589911+jaketbrown@users.noreply.github.com>
2022-10-07 15:39:00 -0400

    Update JsonFlattenerFuzzer.java

--- mayhem/JsonFlattenerFuzzer.java
+++ mayhem/JsonFlattenerFuzzer.java
@@ -10,7 +10,12 @@ import com.github.wnameless.json.flattener.JsonFlattener;
 public class JsonFlattenerFuzzer {
 
   public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    //String json = "{ \"a\" : { \"b\" : 1, \"c\": null, \"d\": [false, true] }, \"e\": \"f\", \"g\":2.3 }";
     String input = data.consumeRemainingAsString();
-    System.out.println(input);  
+    Map<String, Object> flattenJson = JsonFlattener.flattenAsMap(json);
+
+    System.out.println(flattenJson);
+    
+    //System.out.println(input);  
   }
 }
