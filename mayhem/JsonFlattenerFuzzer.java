cca5525d7a9fcd35746d44fe12c62e3f03dee460
jaketbrown <jaketbrown1119@gmail.com>
2022-10-08 20:57:31 -0400

    Add exception catch

--- mayhem/JsonFlattenerFuzzer.java
+++ mayhem/JsonFlattenerFuzzer.java
@@ -2,47 +2,45 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 import java.util.*;
 import java.lang.NumberFormatException;
-
+import java.io.*;
 import com.github.wnameless.json.flattener.*;
 import com.github.wnameless.json.base.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
-
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.github.wnameless.json.unflattener.*;
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 public class JsonFlattenerFuzzer {
-	
-//    public static void fuzzerInitialize(){
-//    	//Test	
-//    	String json = "{ \"a\" : { \"b\" : 1, \"c\": null, \"d\": [false, true] }, \"e\": \"f\", \"g\":2.3 }";
-//    	Map<String, Object> flattenJson = JsonFlattener.flattenAsMap(json);
-//
-//    	System.out.println(flattenJson);
-//    }
+
 
 	public static void fuzzerTestOneInput(FuzzedDataProvider data) throws IOException {
 
-			try {
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode node = mapper.readTree(data.consumeRemainingAsBytes());
-			JacksonJsonValue parsedJacksonJsonValue =  new JacksonJsonValue(node);
-			
-			String nestedJson = JsonUnflattener.unflatten(node.asText());		
-			
-			Map<String, Object> flattenedJson = new JsonFlattener(node.asText()).flattenAsMap();
+		ObjectMapper mapper = new ObjectMapper();
 
-			String jsonStr = JsonFlattener.flatten(node.asText());
-			
-			} catch (JsonProcessingException ignored) {
-				
-			} catch (NumberFormatException e) {
-				
-			}
+		mapper.enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION);
+		mapper.enable(JsonParser.Feature.IGNORE_UNDEFINED);
+		mapper.enable(JsonParser.Feature.ALLOW_YAML_COMMENTS);
+		mapper.enable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);
+		mapper.enable(JsonParser.Feature.ALLOW_SINGLE_QUOTES);
+		mapper.enable(JsonParser.Feature.ALLOW_COMMENTS);
 
-		
+		try {
+			String input = data.consumeRemainingAsString();
+			JsonNode node = mapper.readTree(input);
+
+			String nodeText = node.asText();
+
+			JsonFlattener jf = new JsonFlattener(nodeText);
+			JsonUnflattener ju = new JsonUnflattener(nodeText);
+
+		} catch (Exception ignored) {
+
+		}
+
 	}
 }
