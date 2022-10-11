d2d0f762e9017862adca94cda98eadbdd326980b
Jake Brown <44589911+jaketbrown@users.noreply.github.com>
2022-10-11 18:53:57 -0400

    Update JsonFlattenerFuzzer.java

--- mayhem/JsonFlattenerFuzzer.java
+++ mayhem/JsonFlattenerFuzzer.java
@@ -3,9 +3,6 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 import java.io.IOException;
 
-import com.github.wnameless.json.flattener.JsonFlattener;
-
-
 public class JsonFlattenerFuzzer {
 
 	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
