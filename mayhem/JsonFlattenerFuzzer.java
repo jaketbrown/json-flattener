bf9af9bcc139887327c4136e885349bc40aab5e5
jaketbrown <jaketbrown1119@gmail.com>
2022-10-15 00:03:54 -0400

    Update to fuzzer and corpus

--- mayhem/JsonFlattenerFuzzer.java
+++ mayhem/JsonFlattenerFuzzer.java
@@ -1,24 +1,21 @@
-
-//import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.github.wnameless.json.flattener.*;
 import com.github.wnameless.json.unflattener.*;
-import java.io.StringReader;
 import java.io.IOException;
 
 public class JsonFlattenerFuzzer {
 
 	public static void fuzzerTestOneInput(byte[] data) {
 		String input = new String(data);
-		try {
-		JsonFlattener jf;
-		JsonUnflattener ju;
-		StringReader strReader = new StringReader(input); 
+		System.out.println(input);
+		//try {
+			JsonFlattener jf;
+			JsonUnflattener ju;
 
+			jf = new JsonFlattener(input);
+			String flattened = jf.toString();
+			ju = new JsonUnflattener(input);
+		//} catch (IOException ignored) {
 
-		jf = new JsonFlattener(strReader);
-		ju = new JsonUnflattener(strReader);
-		} catch (IOException ignored) {
-
-		}
+		//}
 	}
 }
