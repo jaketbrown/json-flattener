9b7d891408fb323e55e5a6629322001439d93f2a
jaketbrown <jaketbrown1119@gmail.com>
2022-10-16 13:08:05 -0400

    Number format exception

--- mayhem/JsonFlattenerFuzzer.java
+++ mayhem/JsonFlattenerFuzzer.java
@@ -1,21 +1,36 @@
-import com.github.wnameless.json.flattener.*;
-import com.github.wnameless.json.unflattener.*;
+
+//import com.github.wnameless.json.flattener.*;
+//import com.github.wnameless.json.unflattener.*;
 import java.io.StringReader;
+import java.io.IOException;
+
+import com.github.wnameless.json.flattener.JsonFlattener;
+import com.github.wnameless.json.unflattener.JsonUnflattener;
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 
 public class JsonFlattenerFuzzer {
 
-	public static void fuzzerTestOneInput(byte[] data) {
-		String input = new String(data);
+	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+		String input = data.consumeRemainingAsString();
+		ObjectMapper mapper = new ObjectMapper();
 		try {
-			JsonFlattener jf;
-			JsonUnflattener ju;
-			StringReader strReader = new StringReader(input);
-
-			jf = new JsonFlattener(strReader);
-			ju = new JsonUnflattener(strReader);
-		} catch (IOException ignored) {
-
+			mapper.readTree(input);
+		} catch (JacksonException e) {
+			return;
 		}
+		
+		System.out.println("====\n" + input);
+//		try {
+			//StringReader strReader = new StringReader(input);
+
+			JsonFlattener.flatten(input);
+			JsonUnflattener.unflatten(input);
+//		} catch (Json ignored) {
+//
+//		}
+
 	}
 }
